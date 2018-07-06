373e6d39e37a200ac027dcf223066f286d589eee
teripanda <teri.london@gmail.com>
2018-07-06 14:02:53 -0700

    no REST for the wicked

--- newmelo/newmelo_app/views.py
+++ newmelo/newmelo_app/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from .models import User, Post, Profile, Gspot
 
+from django.forms import ModelForm
+
 # Create your views here.
 
 # USERS
 def userlist(request):
     users = Users.objects.all()
     return render(request, 'newmelo/userlist.html', {'users': users})
+
+
+# ENTRIES
+
+class EntryForm(ModelForm):
+    class Meta:
+        model = post
+        fields = ['id', 'title', 'body', 'user', 'tag', 'created']
+
+def entrylist(request):
+    entries = entry.objects.all()
+    data = {}
