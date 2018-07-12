d7b0167c5f62062cac075dc36d4f9e2ac575073a
teripanda <teri.london@gmail.com>
2018-07-12 13:34:10 -0700

    biniam2

--- newmelo/newmelo_project/settings.py
+++ newmelo/newmelo_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '79@##mk-arj9b^+z@!(2nnj-rf_wgv+kp+!jk+jysq^n2^my1f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
